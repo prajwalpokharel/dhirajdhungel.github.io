cbc40498a5a51941e561c36636a86711c6cc9306
dhirajdhungel <dhirajdhungel@github.com>
2021-05-17 16:32:36 +0545

    Nav bar burger fixing1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar 
   ps= previous scroll position
   cs= current scroll position
-*/
+
 var ps = window.pageYOffset;
 window.onscroll = function() {
   var cs = window.pageYOffset;
@@ -11,4 +11,25 @@ window.onscroll = function() {
     document.getElementByClassName("top-container").style.top = "-50px";
   }
   ps = cs;
-}
+} */
+//nav bar
+const navSlide=() => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.page-nav');
+  const navLinks = document.querySelectorAll('.page-nav a');
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+ 
+  navLinks.forEach((link,index) => {
+    if (link.style.animation){
+      link.style.animation = '';
+    } else {
+        link.style.animation = navLinkFade 0.5s ease forwards $(index / 7 + 0}s;
+    }
+  });
+      burger.classList.toggle('toggle'); 
+
+});
+}   
+navSlide();
