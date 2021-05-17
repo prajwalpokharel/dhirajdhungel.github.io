9ff84319d1905f7a60c09dbb7cffdd6f7df723db
dhirajdhungel <dhirajdhungel@github.com>
2021-05-17 23:34:28 +0545

    Nav bar fixed for mobile devices

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar 
-  ps= previous scroll position
-  cs= current scroll position
 
-var ps = window.pageYOffset;
-window.onscroll = function() {
-  var cs = window.pageYOffset;
-  if (ps > cs) {
-    document.getElementByClassName("top-container").style.top = "0";
-  } else {
-    document.getElementByClassName("top-container").style.top = "-50px";
-  }
-  ps = cs;
-} */
-//nav bar
-const navSlide=() => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.page-nav');
-  const navLinks = document.querySelectorAll('.page-nav a');
+// const navSlide=() => {
+//   const burger = document.querySelector('.burger');
+//   const nav = document.querySelector('.page-nav');
+//   const navLinks = document.querySelectorAll('.page-nav a');
 
-  burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
+//   burger.addEventListener('click', () => {
+//     nav.classList.toggle('nav-active');
  
-  navLinks.forEach((link,index) => {
-    if (link.style.animation){
-      link.style.animation = '';
-    } else {
-        link.style.animation = navLinkFade 0.5s ease forwards $(index / 7 + 0}s;
-    }
-  });
-      burger.classList.toggle('toggle'); 
+//   navLinks.forEach((link,index) => {
+//     if (link.style.animation){
+//       link.style.animation = '';
+//     } else {
+//         link.style.animation = `navLinkFade 0.5s ease forwards $(index / 7 + 0}`;
+//     }
+//   });
+//       burger.classList.toggle('toggle'); 
 
-});
-}   
-navSlide();
+// });
+// }   
+// navSlide();
+
+  const navSlide = function(){
+        var burger = document.querySelector(".burger"),
+            nav = document.querySelector('.page-nav'),
+            navLinks = document.querySelectorAll('.page-nav');
+
+            burger.addEventListener("click", function(){
+            nav.classList.toggle('nav-active');
+
+            navLinks.forEach((link,index) => {
+                if (link.style.animation){
+                link.style.animation = '';
+                } else {
+                    link.style.animation = `navLinkFade 0.3s ease forwards ${index / 3 + 0}s`;
+                }
+            });
+           
+            burger.classList.toggle('toggle'); 
+        });
+    }
+    navSlide();
